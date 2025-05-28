c0d4e3aca1005e877f2c69c40beee646d01f1117
Uliana <uvvorobyova@edu.hse.ru>
2025-05-28 21:26:09 +0300

    Adaptive changes

--- src/javascript/ComponentExample.jsx
+++ src/javascript/ComponentExample.jsx
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+// import '../stylesheets/basics.scss'
 
-export default class ComponentExample extends Component {
-  constructor(props) {
-    super(props)
-  }
+// export default class ComponentExample extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-  render() {
-    return <div className="ComponentExample">This is a React component</div>
-  }
-}
+//   render() {
+//     return <div className="ComponentExample">This is a React component</div>
+//   }
+// }
+
+
+// const t_menu = ReactDOM.createRoot(
+//   document.getElementById('t_menu'),
+// );
+
+// export default function Menu() {
+//   const [isShown, setIsShown] = useState(false);
+
+//   const handleClick = event => {
+//     setIsShown(current => !current);
+//   };
+
+//   return (
+//     <div id="but_t" onClick={handleClick}>
+//       {
+//         isShown && (
+//           <div class="menu_o">
+//             <div class="close"></div>
+//             <a><p>Учебник</p></a>
+//             <a><p>Инструменты</p></a>
+//             <a><p>О проекте</p></a>
+//             <a><p>Telegram</p></a>
+//           </div>
+//         )
+//       }
+//     </div>
+//   );
+// }
+
+// t_menu.render(
+//   <React.StrictMode>
+
+//   </React.StrictMode>
+// )
+
