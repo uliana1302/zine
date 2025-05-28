1643b1762135aa5cf3d1518695d8be83bd3adfb1
Uliana <uvvorobyova@edu.hse.ru>
2025-05-28 21:29:33 +0300

    Merge

---
+++ docs/index.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+ // import jpg from './images/image.jpg'
+// document.addEventListener('DOMContentLoaded', () => {
+//   const image = new Image()
+//   image.src = jpg
+//   document.querySelector('.images').appendChild(image)
+// })
+/******/ })()
+;
